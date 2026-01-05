51210a2d3788dd53859f487b80b87b565f8c20a2
YogenderVermaa <yogender.tech@gmail.com>
2026-01-06 03:30:42 +0530

    updated axios Instance as chnage the hard call to callby reference to env

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 8000,
   headers: {
     'Content-Type': 'application/json',
